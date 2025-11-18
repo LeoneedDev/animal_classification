74795b5f9bf614194fbfefa2065b94d80b364c8e
Mariia Portnykh <79785945+mariik4@users.noreply.github.com>
2025-11-18 16:22:01 +0100

    feat: add dataset, configurationa and eda (#1)

---
+++ dataset/translate.py
@@ -0,0 +1,48 @@
+translate = {
+  "cane": "dog",
+  "cavallo": "horse",
+  "elefante": "elephant",
+  "farfalla": "butterfly",
+  "gallina": "chicken",
+  "gatto": "cat",
+  "mucca": "cow",
+  "pecora": "sheep",
+  "ragno": "spider",
+  "scoiattolo": "squirrel",
+  
+  "dog": "cane", 
+  "horse": "cavallo", 
+  "elephant" : "elefante", 
+  "butterfly": "farfalla",
+  "chicken": "gallina", 
+  "cat": "gatto", 
+  "cow": "mucca", 
+  "spider": "ragno", 
+  "sheep": "pecora",
+  "squirrel": "scoiattolo"}
+
+translate_en_to_it = {
+  "dog": "cane", 
+  "horse": "cavallo", 
+  "elephant" : "elefante", 
+  "butterfly": "farfalla",
+  "chicken": "gallina", 
+  "cat": "gatto", 
+  "cow": "mucca", 
+  "spider": "ragno", 
+  "sheep": "pecora",
+  "squirrel": "scoiattolo"
+}
+
+translate_it_to_en = {
+  "cane": "dog",
+  "cavallo": "horse",
+  "elefante": "elephant",
+  "farfalla": "butterfly",
+  "gallina": "chicken",
+  "gatto": "cat",
+  "mucca": "cow",
+  "pecora": "sheep",
+  "ragno": "spider",
+  "scoiattolo": "squirrel",
+}
