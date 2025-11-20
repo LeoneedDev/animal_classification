86bbe3a06ecebca8320b3cd57ba124813a6cde3f
Name <empty@example.com>
2025-11-20 12:16:37 +0100

    feat: delete to list in ata split

--- image_dataset.py
+++ image_dataset.py
@@ -1,6 +1,8 @@
+import torch
 from torch.utils.data import Dataset
-from PIL import Image
 from torch import Tensor
+from PIL import Image
+import pandas as pd
 # import cv2
 # import numpy as np
 # from PIL import Image
@@ -56,22 +58,22 @@ from torch import Tensor
 #         tensor = self.to_tensor(img_np)
 #         return tensor
 
-from PIL import Image
-
 
 class ImageDataset(Dataset):
-    def __init__(self, image_paths, labels, transform=None):
-        self.image_paths = image_paths
-        self.labels = labels
+    def __init__(self, x: pd.Series, y: pd.Series, transform=None):
+        self.data = pd.DataFrame()
+        self.data['image_path'] = x.copy()
+        self.data['label_encoded'] = y.copy()
         self.transform = transform
 
     def __len__(self):
-        return len(self.image_paths)
+        return len(self.data)
 
     def __getitem__(self, idx):
-        img_path = self.image_paths[idx]
+        img_path = self.data.iloc[idx]['image_path']
         image = Image.open(img_path).convert('RGB')
         if self.transform:
             image = self.transform(image)
-        label = self.labels[idx]
-        return image, label
+        y_label = torch.tensor(int(self.data.iloc[idx]['label_encoded']))
+
+        return (image, y_label)
